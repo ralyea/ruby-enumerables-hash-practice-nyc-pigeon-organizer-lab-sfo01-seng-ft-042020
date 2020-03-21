c9557dfbe1399f7f62f4093d16f804749648cd31
Paul Ralyea <047par@gmail.com>
2020-03-21 19:26:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,35 +1,35 @@
 def nyc_pigeon_organizer(data)
-final = {}
+new = {}
 males = data[:gender][:male]
 females = data[:gender][:female]
 names = males + females
-names.each {|name| final[name] = {}
+names.map {|name| new[name] = {}
 }
 pigeon_color = data[:color]
-pigeon_color.each {|color_pair|
+pigeon_color.map {|color_pair|
   color = color_pair[0]
-data[:color][color].each {|name|
-  p final[name][:color]
-  if final[name][:color].nil?
-final[name][:color] = [color.to_s]
-else final[name][:color] += [color.to_s]
+data[:color][color].map {|name|
+  p new[name][:color]
+  if new[name][:color].nil?
+new[name][:color] = [color.to_s]
+else new[name][:color] += [color.to_s]
 end
 }
 }
 pigeon_gender = data[:gender]
-pigeon_gender.each {|gender_pair|
+pigeon_gender.map {|gender_pair|
 gender = gender_pair[0]
-data[:gender][gender].each {|name|
-final[name][:gender] = [gender.to_s]
+data[:gender][gender].map {|name|
+new[name][:gender] = [gender.to_s]
 }
 }
 pigeon_location = data[:lives]
-pigeon_location.each {|location_pair|
+pigeon_location.map {|location_pair|
   location = location_pair[0]
-  data[:lives][location].each {|name|
-  final[name][:lives] = [location]
+  data[:lives][location].map {|name|
+  new[name][:lives] = [location]
 }
 }
-p final
-final
+p new
+new
 end
