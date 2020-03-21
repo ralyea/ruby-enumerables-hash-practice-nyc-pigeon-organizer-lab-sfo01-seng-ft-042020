00206c1141a376e430e4f4b5fe9d2ac4694ce150
Paul Ralyea <047par@gmail.com>
2020-03-21 19:21:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,35 @@
 def nyc_pigeon_organizer(data)
-new = {}
-i = 0
-while i < data.length
-data[1].collect {|i| new.push[i]   
+final = {}
+males = data[:gender][:male]
+females = data[:gender][:female]
+names = males + females
+names.each {|name| final[name] = {}
 }
+pigeon_color = data[:color]
+pigeon_color.each {|color_pair|
+  color = color_pair[0]
+data[:color][color].each {|name|
+  p final[name][:color]
+  if final[name][:color].nil?
+final[name][:color] = [color.to_s]
+else final[name][:color] += [color.to_s]
 end
+}
+}
+pigeon_gender = data[:gender]
+pigeon_gender.each {|gender_pair|
+gender = gender_pair[0]
+data[:gender][gender].each {|name|
+final[name][:gender] = [gender.to_s]
+}
+}
+pigeon_location = data[:lives]
+pigeon_location.each {|location_pair|
+  location = location_pair[0]
+  data[:lives][location].each {|name|
+  final[name][:lives] = [location]
+}
+}
+p final
+final
 end
