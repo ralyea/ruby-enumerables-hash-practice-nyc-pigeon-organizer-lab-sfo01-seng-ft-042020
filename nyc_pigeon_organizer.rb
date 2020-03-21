3c2c58d5d724dbe61cbe1569bbc889fb20a4d785
Paul Ralyea <047par@gmail.com>
2020-03-21 19:29:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new = {}
+males = data[:gender][:male]
+females = data[:gender][:female]
+names = males + females
+names.map {|name| new[name] = {}
+}
+pigeon_color = data[:color]
+pigeon_color.map {|color_pair|
+  color = color_pair[0]
+data[:color][color].map {|name|
+  p new[name][:color]
+  if new[name][:color].nil?
+new[name][:color] = [color.to_s]
+else new[name][:color] += [color.to_s]
+end
+}
+}
+pigeon_gender = data[:gender]
+pigeon_gender.map {|gender_pair|
+gender = gender_pair[0]
+data[:gender][gender].map {|name|
+new[name][:gender] = [gender.to_s]
+}
+}
+pigeon_location = data[:lives]
+pigeon_location.map {|location_pair|
+  location = location_pair[0]
+  data[:lives][location].map {|name|
+  new[name][:lives] = [location]
+}
+}
+p new
+new
 end
